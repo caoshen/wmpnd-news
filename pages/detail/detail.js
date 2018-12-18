6d26759e0a1c238bc0e5779800c57be30e3e5daf
caoshen <ceeseemail@gmail.com>
2018-12-18 22:56:23 +0800

    first commit

---
+++ pages/detail/detail.js
@@ -0,0 +1,84 @@
+// pages/detail.js
+const host = "https://test-miniprogram.com"
+const detail = "/api/news/detail"
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detail: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let url = host + detail
+    let id = options.id
+    console.log(url + ', ' + id)
+    wx.request({
+      url: url,
+      data: {
+        id: id
+      },
+      success: res => {
+        console.log(res.data.result)
+        let result = res.data.result
+        this.setData({
+          detail: result
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const host = "https://test-miniprogram.com"
+const news = "/api/news/list"
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    newsList: [],
+    activeIndex: 0,
+    newsType: ['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'],
+    newsTypeName: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他']
+  },
+
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.getNewsList()
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  showList: function(result) {
+    console.log(result)
+    let newsList = []
+    let len = result.length
+    for (var i = 0; i < len; ++i) {
+      let date = result[i].date
+      result[i].date = this.convertUTCTimeToLocalTime(date)
+      newsList.push(result[i]);
+    }
+    this.setData({
+      newsList: newsList
+    })
+  },
+  convertUTCTimeToLocalTime: function(UTCTime) {
+    if (!UTCTime) {
+      return '-'
+    }
+    function formatStr(str) {
+      return str > 9 ? str : '0' + str
+    }
+    let date2 = new Date(UTCTime)
+    let hour = date2.getHours()
+    hour = formatStr(hour)
+    let minute = date2.getMinutes()
+    minute = formatStr(minute)
+    return hour + ":" + minute
+  },
+  bindTapIndex: function(event) {
+    let activeIndex = event.currentTarget.dataset.navIndex
+    this.setData({
+      activeIndex: activeIndex
+    })
+    this.getNewsList()
+  },
+  getNewsList: function() {
+    let url = host + news
+    let typeList = this.data.newsType
+    let type = typeList[this.data.activeIndex]
+    console.log(url + ', ' + type)
+    wx.request({
+      url: url,
+      data: {
+        type: type
+      },
+      success: res => {
+        console.log(res.data)
+        let result = res.data.result;
+        this.showList(result)
+      }
+    })
+  },
+  bindTapId: function(event) {
+    let id = event.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '../detail/detail?id=' + id
+    })
+  }
+})
